9263ad3af351e4403c6c8110dca1fbb8f01885b0
Zeeshan Zafar <zeeshanzafar922@gmail.com>
2024-01-03 18:14:41 +0500

    pos module bugs fix

--- resources/views/admin/pos/index.blade.php
+++ resources/views/admin/pos/index.blade.php
@@ -11,6 +11,16 @@
                 Home - <span class="fw-normal">POS</span>
             </h4>
         </div>
+        <div class="d-lg-block my-lg-auto ms-lg-auto">
+            <div class="d-sm-flex align-items-center mb-3 mb-lg-0 ms-lg-3">
+                <a href="{{ route('invoices.index') }}" class="btn btn-outline-primary btn-labeled btn-labeled-start rounded-pill">
+                    <span class="btn-labeled-icon bg-primary text-white rounded-pill">
+                        <i class="ph-arrow-circle-left"></i>
+                    </span>
+                    Back
+                </a>
+            </div>
+        </div>
     </div>
 @endsection
 
